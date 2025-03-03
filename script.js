7c1017ead4394cd2dac6b410005c16691c79ec7a
EnesXVC <ien3ssq@gmail.com>
2025-03-03 06:56:55 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    alert('Button tıklandı!');
+document.getElementById('script1').addEventListener('click', function() {
+    document.getElementById('link').value = 'https://example.com/script1';
+});
+
+document.getElementById('script2').addEventListener('click', function() {
+    document.getElementById('link').value = 'https://example.com/script2';
+});
+
+document.getElementById('script3').addEventListener('click', function() {
+    document.getElementById('link').value = 'https://example.com/script3';
+});
+
+document.getElementById('script4').addEventListener('click', function() {
+    document.getElementById('link').value = 'https://example.com/script4';
+});
+
+document.getElementById('script5').addEventListener('click', function() {
+    document.getElementById('link').value = 'https://example.com/script5';
 });
